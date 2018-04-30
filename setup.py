28f7c9f6508725491a660fe4c1759e5811218735
Chris Caron <lead2gold@gmail.com>
2018-04-30 16:40:00 -0400

    added pypi support

---
+++ setup.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Datetools provide a method of manipulating and working dates and times.
+# Copyright (C) 2013-2018 Chris Caron <lead2gold@gmail.com>
+#
+# This file is part of Datetools.  Datetools is free software; you can
+# redistribute it and/or modify it under the terms of the GNU General Public
+# License as published by the Free Software Foundation; either version 2 of
+# the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+from distutils.core import setup, Extension
+
+setup(
+    name = "dateblock",
+    version = "1.0.0",
+    url='https://github.com/caronc/datetools/',
+    description="""
+        Python Cron-like date mainpulation
+    """.strip(),
+    author='Chris Caron',
+    author_email='lead2gold@gmail.com',
+    license="GPLv2",
+    classifiers=(
+                'Development Status :: 5 - Production/Stable',
+                'Intended Audience :: Developers',
+                'Operating System :: POSIX :: Linux',
+                'Natural Language :: English',
+                'Programming Language :: C++',
+                'Programming Language :: Python',
+                'Programming Language :: Python :: 2.7',
+                'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
+            ),
+    ext_modules = [
+        Extension("dateblock", ["src/Date.cpp", "src/dateblock.cpp"],
+                  libraries=['stdc++'],
+                  define_macros=[
+                      ('PYTHON_MODULE', '1'),
+                  ])],
+    keywords='date sleep block time manipulation cron',
+)
